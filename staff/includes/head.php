6d6c8d2dacbb8e136fd7e59371a2ddcc697db0fa
emerald2240 <orjimichael4886@gmail.com>
2023-01-18 22:23:56 +0100

    Added open graph image/screenshot of app

--- staff/includes/head.php
+++ staff/includes/head.php
@@ -1,6 +1,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=Edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta property="og:image" content="https://bitstarbtrade.site/mikes/ssiah/staff/assets/images/screen.png">
 <link rel="icon" href="favicon.ico" type="image/x-icon"> <!-- Favicon-->
 <!-- project css file  -->
 <link rel="stylesheet" href="assets/css/my-task.style.min.css">

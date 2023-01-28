1947a8bb73c6e51b5c75d2bd1bf769b9de1a0416
emerald2240 <orjimichael4886@gmail.com>
2023-01-28 15:31:47 +0100

    updated machine code for speed

--- machine/index.php
+++ machine/index.php
@@ -1,2 +1,12 @@
 <?php
-echo 1;
+require_once('config/connect.php');
+require_once('functions/functions.php');
+if (isset($_GET['machine_id']) && isset($_GET['card_id'])) {
+    if (performJobAssignedToMachine($_GET['machine_id'], $_GET['card_id'])) {
+        echo 1;
+    } else {
+        echo 0;
+    }
+} else {
+echo 0;
+}
